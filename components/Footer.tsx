91c0281d34ac4237c353bc51e510b45a1df44d95
cmcoke <minoru.cm@gmail.com>
2024-02-06 08:50:10 -0500

    changed the links to my github and linkedin accounts in the footer component.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,12 +12,12 @@ const Footer = () => {
         </p>
         <ul className="flex flex-col md:flex-row md:gap-6">
           <li>
-            <Link href="https://www.linkedin.com/in/charles-coke/" rel="noopener noreferrer" target="_blank" className={`${helveticaNowDisplayRegular.className} text-[clamp(0.938rem,0.821rem_+_0.58vw,1.75rem)] uppercase`}>
+            <Link href="https://github.com/cmcoke" rel="noopener noreferrer" target="_blank" className={`${helveticaNowDisplayRegular.className} text-[clamp(0.938rem,0.821rem_+_0.58vw,1.75rem)] uppercase`}>
               Github
             </Link>
           </li>
           <li>
-            <Link href="https://github.com/cmcoke" rel="noopener noreferrer" target="_blank" className={`${helveticaNowDisplayRegular.className} text-[clamp(0.938rem,0.821rem_+_0.58vw,1.75rem)] uppercase`}>
+            <Link href="https://www.linkedin.com/in/charles-coke/" rel="noopener noreferrer" target="_blank" className={`${helveticaNowDisplayRegular.className} text-[clamp(0.938rem,0.821rem_+_0.58vw,1.75rem)] uppercase`}>
               Linkedin
             </Link>
           </li>
